0919d1ce46dbeff2e03960cb490196f55e0b0abe
Owen Meier <owen2717@gmail.com>
2025-07-03 18:08:29 -0500

    adding conditional coloring to checkerboard the board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,9 +5,9 @@ export default function Board() {
   return (
     <div>
       {Array.from({ length: 8 }).map((_, i) => (
-        <div className="flex">
+        <div className="flex" key={i}>
           {Array.from({ length: 8 }).map((_, j) => (
-            <Square key={`${i}.${j}`} />
+            <Square key={`${i}.${j}`} row={i} col={j} />
           ))}
         </div>
       ))}
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Square() {
-  return <div className="w-[50px] h-[50px] bg-gray-300"></div>;
+export default function Square({ row, col }) {
+  const isDark =
+    (row % 2 == 0 && col % 2 == 1) || (row % 2 == 1 && col % 2 == 0);
+  const color = isDark ? "bg-blue-400" : "bg-gray-50";
+  //when row is even, then isDark == true when col is odd
+  // when row is odd, then isDark == true when col is even
+
+  return <div className={`w-[50px] h-[50px] ${color}`}></div>;
 }
