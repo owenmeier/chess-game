0da0376b6683e309119a2b2733a3d0ac19fee690
Owen Meier <owen2717@gmail.com>
2025-07-03 18:10:44 -0500

    corrected color conditioning so board is correct now

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Square({ row, col }) {
   const isDark =
-    (row % 2 == 0 && col % 2 == 1) || (row % 2 == 1 && col % 2 == 0);
+    (row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1);
   const color = isDark ? "bg-blue-400" : "bg-gray-50";
   //when row is even, then isDark == true when col is odd
   // when row is odd, then isDark == true when col is even
