963283638fc830e74edd31d233b5acce35eeaaa3
Owen Meier <owen2717@gmail.com>
2025-07-03 18:15:32 -0500

    changed board colors so they're more natural for a basic chess board

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Square({ row, col }) {
   const isDark =
     (row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1);
-  const color = isDark ? "bg-blue-400" : "bg-gray-50";
+  const color = isDark ? "bg-[#9c6b41]" : "bg-[#faebd2]";
   //when row is even, then isDark == true when col is odd
   // when row is odd, then isDark == true when col is even
 
