014caaa8704e7e668c8ebd1bb02f0004a732fc60
Owen Meier <owen2717@gmail.com>
2025-07-06 18:50:47 -0500

    added createpiece function and setupboard function to keep track of board state

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import Square from "./Square";
 
+// id syntax:
+// leading digit is type of piece (rook, bishop, knight etc) defined by value of piece
+// i.e. rooks are worth 5 points, knights 3, bishops 3, etc
+// for the purpose of this syntax knights will be worth 3 and bishops 4
+// second digit will be which # of each piece the piece is (rooks 1 and 2, pawns 1-8)
+// final digit will be piece color, 1 for black 2 for white
+function createPiece(name, color, id) {
+  return { name, color, id };
+}
+
+const board = Array.from({ length: 8 }, (_, row) =>
+  Array.from({ lenfth: 8 }, (_, col) => null)
+);
+
+function setupBoard() {
+  board[0][0] = createPiece("rook", "black", 510);
+}
+
 export default function Board() {
   return (
     <div>
