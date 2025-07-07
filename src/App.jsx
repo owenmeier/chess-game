74a377f38d210f7e50ebddee23b9106e15c620e8
Owen Meier <owen2717@gmail.com>
2025-07-07 16:33:58 -0500

    minor change centered board

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen">
       <Board />
     </div>
   );
