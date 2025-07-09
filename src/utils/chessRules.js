e5e9e6a4b280c154e4b06171efd859020c2e92cc
Owen Meier <owen2717@gmail.com>
2025-07-09 14:17:15 -0500

    almost done with rook logic, moving is all fine

--- src/utils/chessRules.js
+++ src/utils/chessRules.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
 function pawnAttempt(board, selectedPiece, curPos, targetPos) {
-  const [fromRow, fromCol] = curPos;
-  const [toRow, toCol] = targetPos;
+  const [fromRow, fromCol] = curPos; // current position of piece
+  const [toRow, toCol] = targetPos; // target position of piece
 
   const direction = selectedPiece.color == "white" ? -1 : 1;
   const startRow = selectedPiece.color == "white" ? 6 : 1;
@@ -14,9 +14,7 @@ function pawnAttempt(board, selectedPiece, curPos, targetPos) {
     !board[toRow][toCol]
   ) {
     return true;
-  }
-
-  if (
+  } else if (
     // double move on first move
     fromRow == startRow &&
     toCol == fromCol &&
@@ -25,9 +23,7 @@ function pawnAttempt(board, selectedPiece, curPos, targetPos) {
     !board[toRow][toCol]
   ) {
     return true;
-  }
-
-  if (
+  } else if (
     // capture enemy piece
     Math.abs(fromCol - toCol) == 1 &&
     toRow == fromRow + direction &&
@@ -43,6 +39,34 @@ function pawnAttempt(board, selectedPiece, curPos, targetPos) {
 function rookAttempt(board, selectedPiece, curPos, targetPos) {
   const [fromRow, fromCol] = curPos;
   const [toRow, toCol] = targetPos;
+
+  // making sure that rook is only moving in straight lines
+  if (fromRow !== toRow && fromCol !== toCol) {
+    return false;
+  } else {
+    const numSpaces = Math.max(
+      Math.abs(fromRow - toRow),
+      Math.abs(fromCol - toCol)
+    );
+    const rowStep = fromRow == toRow ? 0 : toRow > fromRow ? 1 : -1;
+    const colStep = fromCol == toCol ? 0 : toCol > fromCol ? 1 : -1;
+    console.log(numSpaces);
+
+    for (let i = 1; i < numSpaces; i++) {
+      console.log(i);
+      if (board[fromRow + rowStep * i][fromCol + colStep * i]) {
+        // check if path is blocked
+        return false;
+      }
+    }
+    if (
+      board[toRow][toCol] ==
+        board[fromRow + numSpaces * rowStep][fromCol + numSpaces * colStep] &&
+      board[toRow][toCol].color !== selectedPiece.color
+    ) {
+      return true;
+    }
+  }
 
   return false;
 }
