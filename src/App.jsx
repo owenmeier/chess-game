22fa39fe92faa7ec11c28917953fc0adda203e86
Owen Meier <owen2717@gmail.com>
2025-07-12 15:51:18 -0500

    added grid and flex to make space for movehistory

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { Board, Square } from "./components/index.jsx";
+import { Board, MoveHistory } from "./components/index.jsx";
 import React from "react";
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Board />
-    </div>
+    <>
+      <div className="grid grid-cols-[1fr_auto_1fr] gap-4 min-h-screen">
+        <div></div>
+        <div>
+          <Board className="flex justify-center items-center" />
+        </div>
+        <div className="flex pt-4">
+          <div>
+            <MoveHistory />
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ src/components/MoveHistory.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Board from "./Board";
+
+export default function MoveHistory() {
+  return <div>Location Test</div>;
+}
