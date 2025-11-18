1294492603b586973981ee6ced3426fb849a719e
Owen Meier <owen2717@gmail.com>
2025-11-17 23:29:13 -0600

    no changes

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import Piece from "./Piece";
 
 export default function Square({ row, col, piece, highlighted, onClick }) {
-  const isDark =
-    (row % 2 == 1 && col % 2 == 0) || (row % 2 == 0 && col % 2 == 1);
-  const color = isDark ? "bg-[#a8744a]" : "bg-[#faebd2]";
-  //when row is even, then isDark == true when col is odd
-  // when row is odd, then isDark == true when col is even
+	const isDark =
+		(row % 2 == 1 && col % 2 == 0) || (row % 2 == 0 && col % 2 == 1);
+	const color = isDark ? "bg-[#a8744a]" : "bg-[#faebd2]";
+	//when row is even, then isDark == true when col is odd
+	// when row is odd, then isDark == true when col is even
 
-  return (
-    <div
-      className={`w-[50px] h-[50px] ${color} ${
-        highlighted ? "bg-[#fce174]" : ""
-      }`}
-      onClick={onClick}
-    >
-      {piece && <Piece {...piece} key={piece.id} />}
-    </div>
-  );
+	return (
+		<div
+			className={`w-[50px] h-[50px] ${color} ${
+				highlighted ? "bg-[#fce174]" : ""
+			}`}
+			onClick={onClick}
+		>
+			{piece && <Piece {...piece} key={piece.id} />}
+		</div>
+	);
 }
