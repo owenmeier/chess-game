1dd6606b195ff9a64a5b3095060f844885d36760
Owen Meier <owen2717@gmail.com>
2025-11-17 22:37:26 -0600

    added castling function, next step is making sure notation is correct

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,168 +3,182 @@ import Square from "./Square";
 import isLegalMove from "../utils/chessRules";
 
 const PIECES = {
-  PAWN: "pawn",
-  ROOK: "rook",
-  KNIGHT: "knight",
-  BISHOP: "bishop",
-  QUEEN: "queen",
-  KING: "king",
+	PAWN: "pawn",
+	ROOK: "rook",
+	KNIGHT: "knight",
+	BISHOP: "bishop",
+	QUEEN: "queen",
+	KING: "king",
 };
 const COLORS = {
-  WHITE: "white",
-  BLACK: "black",
+	WHITE: "white",
+	BLACK: "black",
 };
 
 function createPiece(name, color) {
-  const piece = { name, color };
-  return piece;
+	const piece = { name, color, hasMoved: false };
+	return piece;
 }
 
 export function getInitialBoard() {
-  const board = Array(8)
-    .fill(null)
-    .map(() => Array(8).fill(null));
+	const board = Array(8)
+		.fill(null)
+		.map(() => Array(8).fill(null));
 
-  const pieceOrder = [
-    PIECES.ROOK,
-    PIECES.KNIGHT,
-    PIECES.BISHOP,
-    PIECES.QUEEN,
-    PIECES.KING,
-    PIECES.BISHOP,
-    PIECES.KNIGHT,
-    PIECES.ROOK,
-  ];
+	const pieceOrder = [
+		PIECES.ROOK,
+		PIECES.KNIGHT,
+		PIECES.BISHOP,
+		PIECES.QUEEN,
+		PIECES.KING,
+		PIECES.BISHOP,
+		PIECES.KNIGHT,
+		PIECES.ROOK,
+	];
 
-  for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 8; j++) {
-      const row = i == 0 ? 0 : 7;
-      const color = i == 0 ? COLORS.BLACK : COLORS.WHITE;
-      const pawnRow = color == COLORS.BLACK ? 1 : 6;
-      board[row][j] = createPiece(pieceOrder[j], color);
-      board[pawnRow][j] = createPiece(PIECES.PAWN, color);
-    }
-  }
+	for (let i = 0; i < 2; i++) {
+		for (let j = 0; j < 8; j++) {
+			const row = i == 0 ? 0 : 7;
+			const color = i == 0 ? COLORS.BLACK : COLORS.WHITE;
+			const pawnRow = color == COLORS.BLACK ? 1 : 6;
+			board[row][j] = createPiece(pieceOrder[j], color);
+			board[pawnRow][j] = createPiece(PIECES.PAWN, color);
+		}
+	}
 
-  return board;
+	return board;
 }
 
 export default function Board({
-  moveHistory,
-  setMoveHistory,
-  board,
-  setBoard,
-  turn,
-  setTurn,
-  curMoveIndex,
-  setCurMoveIndex,
+	moveHistory,
+	setMoveHistory,
+	board,
+	setBoard,
+	turn,
+	setTurn,
+	curMoveIndex,
+	setCurMoveIndex,
 }) {
-  // const [board, setBoard] = useState(getInitialBoard); // board state
-  const [selected, setSelected] = useState(null); // selected piece state
-  // const [turn, setTurn] = useState(COLORS.WHITE); // turn state
-  // const [moveHistory, setMoveHistory] = useState([]); // last move info including pawn doubleStep
+	// const [board, setBoard] = useState(getInitialBoard); // board state
+	const [selected, setSelected] = useState(null); // selected piece state
+	// const [turn, setTurn] = useState(COLORS.WHITE); // turn state
+	// const [moveHistory, setMoveHistory] = useState([]); // last move info including pawn doubleStep
 
-  // move function
-  function executeMove(selected, row, col) {
-    const newBoard = board.map((row) => row.slice()); // create temp new board
-    const lastMove = moveHistory[moveHistory.length - 1];
+	// move function
+	function executeMove(selected, row, col) {
+		const newBoard = board.map((row) => row.slice()); // create temp new board
+		const lastMove = moveHistory[moveHistory.length - 1];
 
-    newBoard[row][col] = selected.piece; //
-    newBoard[selected.row][selected.col] = null;
+		const movedPiece = { ...selected.piece, hasMoved: true }; // says piece has now moved
+		newBoard[row][col] = selected.piece; //
+		newBoard[selected.row][selected.col] = null;
 
-    const isEnPassant =
-      selected.piece.name == PIECES.PAWN &&
-      Math.abs(selected.col - col) == 1 &&
-      board[row][col] == null &&
-      lastMove &&
-      lastMove.piece &&
-      lastMove.piece.name == PIECES.PAWN &&
-      lastMove.piece.color !== selected.piece.color &&
-      lastMove.doubleStep &&
-      lastMove.toRow == row + (selected.piece.color == COLORS.WHITE ? 1 : -1) &&
-      lastMove.toCol == col;
+		const isEnPassant =
+			selected.piece.name == PIECES.PAWN &&
+			Math.abs(selected.col - col) == 1 &&
+			board[row][col] == null &&
+			lastMove &&
+			lastMove.piece &&
+			lastMove.piece.name == PIECES.PAWN &&
+			lastMove.piece.color !== selected.piece.color &&
+			lastMove.doubleStep &&
+			lastMove.toRow == row + (selected.piece.color == COLORS.WHITE ? 1 : -1) &&
+			lastMove.toCol == col;
 
-    if (isEnPassant) {
-      newBoard[selected.row][col] = null;
-    }
+		if (isEnPassant) {
+			newBoard[selected.row][col] = null;
+		}
 
-    return newBoard;
-  }
+		const isCastling =
+			selected.piece.name == PIECES.KING && Math.abs(selected.col - col) == 2;
 
-  // when you click a square, you get the row and column of the square
-  function handleSquareClick(row, col) {
-    // first checking if a piece is selected, to then attempt a move
-    // console.log(lastMove);
+		if (isCastling) {
+			const isKingSide = col > selected.col;
+			const rookCol = isKingSide ? 7 : 0;
+			const newRookCol = isKingSide ? col - 1 : col + 1;
 
-    // first we check if the selected state is true (if a piece is already selected)
-    if (
-      // check if we have a piece selected
-      selected &&
-      // check if selected piece is the color of which turn it is
-      selected.piece.color == turn &&
-      // if a piece is already selected, we check if the new square we clicked is a legal move
-      // for the piece that we have selected
-      isLegalMove(
-        moveHistory[moveHistory.length - 1], // lastMove state
-        board, // board state
-        selected.piece, // selectedPiece
-        [selected.row, selected.col], // curPos
-        [row, col] // targetPos
-      )
-    ) {
-      const newBoard = executeMove(selected, row, col);
-      let newMoveHistory;
+			const rook = newBoard[row][rookCol];
+			newBoard[row][newRookCol] = { ...rook, hasMoved: true };
+			newBoard[row][rookCol] = null;
+		}
 
-      if (curMoveIndex == -1) {
-        newMoveHistory = [...moveHistory];
-      } else {
-        newMoveHistory = moveHistory.slice(0, curMoveIndex + 1);
-      }
+		return newBoard;
+	}
 
-      newMoveHistory.push({
-        board: newBoard,
-        turn: turn,
-        piece: selected.piece,
-        fromRow: selected.row,
-        fromCol: selected.col,
-        toRow: row,
-        toCol: col,
-        doubleStep:
-          selected.piece.name == PIECES.PAWN &&
-          Math.abs(selected.row - row) == 2,
-      });
+	// when you click a square, you get the row and column of the square
+	function handleSquareClick(row, col) {
+		// first checking if a piece is selected, to then attempt a move
+		// console.log(lastMove);
 
-      setMoveHistory(newMoveHistory);
-      setCurMoveIndex(-1);
-      setBoard(newBoard); // update board with newBoard from executeMove()
-      setSelected(null); // deselect everything
-      setTurn(turn == COLORS.WHITE ? COLORS.BLACK : COLORS.WHITE); // toggle turn
-    } else if (
-      board[row][col] &&
-      (!selected || board[selected.row][selected.col] !== board[row][col])
-    ) {
-      setSelected({ row, col, piece: board[row][col] }); // if piece, select it
-    } else {
-      setSelected(null); // otherwise, deselect everything
-    }
-  }
+		// first we check if the selected state is true (if a piece is already selected)
+		if (
+			// check if we have a piece selected
+			selected &&
+			// check if selected piece is the color of which turn it is
+			selected.piece.color == turn &&
+			// if a piece is already selected, we check if the new square we clicked is a legal move
+			// for the piece that we have selected
+			isLegalMove(
+				moveHistory[moveHistory.length - 1], // lastMove state
+				board, // board state
+				selected.piece, // selectedPiece
+				[selected.row, selected.col], // curPos
+				[row, col] // targetPos
+			)
+		) {
+			const newBoard = executeMove(selected, row, col);
+			let newMoveHistory;
 
-  return (
-    <div>
-      {board.map((rowArr, i) => (
-        <div className="flex" key={i}>
-          {rowArr.map((piece, j) => (
-            <Square
-              key={`${i}${j}`}
-              row={i}
-              col={j}
-              piece={piece}
-              onClick={() => handleSquareClick(i, j)}
-              highlighted={selected && selected.row == i && selected.col == j}
-            />
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+			if (curMoveIndex == -1) {
+				newMoveHistory = [...moveHistory];
+			} else {
+				newMoveHistory = moveHistory.slice(0, curMoveIndex + 1);
+			}
+
+			newMoveHistory.push({
+				board: newBoard,
+				turn: turn,
+				piece: selected.piece,
+				fromRow: selected.row,
+				fromCol: selected.col,
+				toRow: row,
+				toCol: col,
+				doubleStep:
+					selected.piece.name == PIECES.PAWN &&
+					Math.abs(selected.row - row) == 2,
+			});
+
+			setMoveHistory(newMoveHistory);
+			setCurMoveIndex(-1);
+			setBoard(newBoard); // update board with newBoard from executeMove()
+			setSelected(null); // deselect everything
+			setTurn(turn == COLORS.WHITE ? COLORS.BLACK : COLORS.WHITE); // toggle turn
+		} else if (
+			board[row][col] &&
+			(!selected || board[selected.row][selected.col] !== board[row][col])
+		) {
+			setSelected({ row, col, piece: board[row][col] }); // if piece, select it
+		} else {
+			setSelected(null); // otherwise, deselect everything
+		}
+	}
+
+	return (
+		<div>
+			{board.map((rowArr, i) => (
+				<div className="flex" key={i}>
+					{rowArr.map((piece, j) => (
+						<Square
+							key={`${i}${j}`}
+							row={i}
+							col={j}
+							piece={piece}
+							onClick={() => handleSquareClick(i, j)}
+							highlighted={selected && selected.row == i && selected.col == j}
+						/>
+					))}
+				</div>
+			))}
+		</div>
+	);
 }
--- src/utils/chessRules.js
+++ src/utils/chessRules.js
@@ -1,174 +1,204 @@
 const PIECES = {
-  PAWN: "pawn",
-  ROOK: "rook",
-  KNIGHT: "knight",
-  BISHOP: "bishop",
-  QUEEN: "queen",
-  KING: "king",
+	PAWN: "pawn",
+	ROOK: "rook",
+	KNIGHT: "knight",
+	BISHOP: "bishop",
+	QUEEN: "queen",
+	KING: "king",
 };
 const COLORS = {
-  WHITE: "white",
-  BLACK: "black",
+	WHITE: "white",
+	BLACK: "black",
 };
 
 function pawnAttempt(lastMove, board, selectedPiece, curPos, targetPos) {
-  const [fromRow, fromCol] = curPos; // current position of piece
-  const [toRow, toCol] = targetPos; // target position of piece
-  const direction = selectedPiece.color == COLORS.WHITE ? -1 : 1; // direction of pawn movement
-  const startRow = selectedPiece.color == COLORS.WHITE ? 6 : 1; // starting row of pawn
+	const [fromRow, fromCol] = curPos; // current position of piece
+	const [toRow, toCol] = targetPos; // target position of piece
+	const direction = selectedPiece.color == COLORS.WHITE ? -1 : 1; // direction of pawn movement
+	const startRow = selectedPiece.color == COLORS.WHITE ? 6 : 1; // starting row of pawn
 
-  if (
-    // single move
-    toCol == fromCol &&
-    fromRow + direction == toRow &&
-    !board[toRow][toCol]
-  ) {
-    return true;
-  } else if (
-    // double move on first move
-    fromRow == startRow &&
-    toCol == fromCol &&
-    fromRow + 2 * direction == toRow &&
-    !board[fromRow + direction][fromCol] &&
-    !board[toRow][toCol]
-  ) {
-    return true;
-  } else if (
-    // capture enemy piece
-    Math.abs(fromCol - toCol) == 1 &&
-    toRow == fromRow + direction &&
-    board[toRow][toCol] &&
-    board[toRow][toCol].color !== selectedPiece.color
-  ) {
-    return true;
-  } else if (
-    Math.abs(fromCol - toCol) == 1 &&
-    board[toRow][toCol] == null &&
-    lastMove &&
-    lastMove.piece &&
-    lastMove.piece.name == PIECES.PAWN &&
-    lastMove.piece.color !== selectedPiece.color &&
-    lastMove.doubleStep &&
-    toRow == fromRow + direction &&
-    toCol == lastMove.fromCol
-  ) {
-    return true;
-  }
+	if (
+		// single move
+		toCol == fromCol &&
+		fromRow + direction == toRow &&
+		!board[toRow][toCol]
+	) {
+		return true;
+	} else if (
+		// double move on first move
+		fromRow == startRow &&
+		toCol == fromCol &&
+		fromRow + 2 * direction == toRow &&
+		!board[fromRow + direction][fromCol] &&
+		!board[toRow][toCol]
+	) {
+		return true;
+	} else if (
+		// capture enemy piece
+		Math.abs(fromCol - toCol) == 1 &&
+		toRow == fromRow + direction &&
+		board[toRow][toCol] &&
+		board[toRow][toCol].color !== selectedPiece.color
+	) {
+		return true;
+	} else if (
+		Math.abs(fromCol - toCol) == 1 &&
+		board[toRow][toCol] == null &&
+		lastMove &&
+		lastMove.piece &&
+		lastMove.piece.name == PIECES.PAWN &&
+		lastMove.piece.color !== selectedPiece.color &&
+		lastMove.doubleStep &&
+		toRow == fromRow + direction &&
+		toCol == lastMove.fromCol
+	) {
+		return true;
+	}
 
-  return false;
+	return false;
 }
 
 function rookAttempt(board, selectedPiece, curPos, targetPos) {
-  const [fromRow, fromCol] = curPos;
-  const [toRow, toCol] = targetPos;
+	const [fromRow, fromCol] = curPos;
+	const [toRow, toCol] = targetPos;
 
-  // making sure that rook is only moving in straight lines
-  if (fromRow !== toRow && fromCol !== toCol) {
-    return false;
-  }
-  const steps = Math.max(Math.abs(fromRow - toRow), Math.abs(fromCol - toCol));
-  const rowStep = fromRow == toRow ? 0 : toRow > fromRow ? 1 : -1;
-  const colStep = fromCol == toCol ? 0 : toCol > fromCol ? 1 : -1;
+	// making sure that rook is only moving in straight lines
+	if (fromRow !== toRow && fromCol !== toCol) {
+		return false;
+	}
+	const steps = Math.max(Math.abs(fromRow - toRow), Math.abs(fromCol - toCol));
+	const rowStep = fromRow == toRow ? 0 : toRow > fromRow ? 1 : -1;
+	const colStep = fromCol == toCol ? 0 : toCol > fromCol ? 1 : -1;
 
-  for (let i = 1; i < steps; i++) {
-    if (board[fromRow + rowStep * i][fromCol + colStep * i]) return false; // check if path blocked
-  }
-  if (
-    !board[toRow][toCol] ||
-    board[toRow][toCol].color !== selectedPiece.color
-  ) {
-    return true;
-  }
+	for (let i = 1; i < steps; i++) {
+		if (board[fromRow + rowStep * i][fromCol + colStep * i]) return false; // check if path blocked
+	}
+	if (
+		!board[toRow][toCol] ||
+		board[toRow][toCol].color !== selectedPiece.color
+	) {
+		return true;
+	}
 
-  return false;
+	return false;
 }
 
 function knightAttempt(board, selectedPiece, curPos, targetPos) {
-  const [fromRow, fromCol] = curPos;
-  const [toRow, toCol] = targetPos;
+	const [fromRow, fromCol] = curPos;
+	const [toRow, toCol] = targetPos;
 
-  if (
-    (Math.abs(fromRow - toRow) == 2 && Math.abs(fromCol - toCol) == 1) ||
-    (Math.abs(fromCol - toCol) == 2 && Math.abs(fromRow - toRow) == 1)
-  ) {
-    if (
-      !board[toRow][toCol] ||
-      board[toRow][toCol].color !== selectedPiece.color
-    ) {
-      return true;
-    }
-  }
+	if (
+		(Math.abs(fromRow - toRow) == 2 && Math.abs(fromCol - toCol) == 1) ||
+		(Math.abs(fromCol - toCol) == 2 && Math.abs(fromRow - toRow) == 1)
+	) {
+		if (
+			!board[toRow][toCol] ||
+			board[toRow][toCol].color !== selectedPiece.color
+		) {
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
 
 function bishopAttempt(board, selectedPiece, curPos, targetPos) {
-  const [fromRow, fromCol] = curPos;
-  const [toRow, toCol] = targetPos;
+	const [fromRow, fromCol] = curPos;
+	const [toRow, toCol] = targetPos;
 
-  // making sure bishop is moving exactly diagonally (row and col must change same amount)
-  if (Math.abs(fromRow - toRow) !== Math.abs(fromCol - toCol)) {
-    return false;
-  }
-  const rowStep = toRow > fromRow ? 1 : -1;
-  const colStep = toCol > fromCol ? 1 : -1;
-  const steps = Math.abs(fromRow - toRow);
+	// making sure bishop is moving exactly diagonally (row and col must change same amount)
+	if (Math.abs(fromRow - toRow) !== Math.abs(fromCol - toCol)) {
+		return false;
+	}
+	const rowStep = toRow > fromRow ? 1 : -1;
+	const colStep = toCol > fromCol ? 1 : -1;
+	const steps = Math.abs(fromRow - toRow);
 
-  for (let i = 1; i < steps; i++) {
-    if (board[fromRow + rowStep * i][fromCol + colStep * i]) return false; // check if path blocked
-  }
-  if (
-    !board[toRow][toCol] ||
-    board[toRow][toCol].color !== selectedPiece.color
-  ) {
-    return true;
-  }
+	for (let i = 1; i < steps; i++) {
+		if (board[fromRow + rowStep * i][fromCol + colStep * i]) return false; // check if path blocked
+	}
+	if (
+		!board[toRow][toCol] ||
+		board[toRow][toCol].color !== selectedPiece.color
+	) {
+		return true;
+	}
 
-  return false;
+	return false;
 }
 
 function queenAttempt(board, selectedPiece, curPos, targetPos) {
-  return (
-    bishopAttempt(board, selectedPiece, curPos, targetPos) ||
-    rookAttempt(board, selectedPiece, curPos, targetPos)
-  );
+	return (
+		bishopAttempt(board, selectedPiece, curPos, targetPos) ||
+		rookAttempt(board, selectedPiece, curPos, targetPos)
+	);
 }
 
 function kingAttempt(board, selectedPiece, curPos, targetPos) {
-  // TODO incomplete since need to add special rules for checks and checkmate
-  const [fromRow, fromCol] = curPos;
-  const [toRow, toCol] = targetPos;
+	// TODO incomplete since need to add special rules for checks and checkmate
+	const [fromRow, fromCol] = curPos;
+	const [toRow, toCol] = targetPos;
 
-  if (Math.abs(fromRow - toRow) <= 1 && Math.abs(fromCol - toCol) <= 1) {
-    return (
-      !board[toRow][toCol] || board[toRow][toCol].color !== selectedPiece.color
-    );
-  }
+	// if not castling (only moving 1 space)
+	if (Math.abs(fromRow - toRow) <= 1 && Math.abs(fromCol - toCol) <= 1) {
+		return (
+			!board[toRow][toCol] || board[toRow][toCol].color !== selectedPiece.color
+		);
+	}
 
-  return false;
+	// castling (moving 2 squares)
+	if (
+		!selectedPiece.hasMoved &&
+		fromRow == toRow &&
+		Math.abs(fromCol - toCol) == 2
+	) {
+		const isKingSide = toCol > fromCol;
+		const rookCol = isKingSide ? 7 : 0;
+		const rook = board[fromRow][rookCol];
+
+		if (
+			!rook ||
+			rook.name !== PIECES.ROOK ||
+			rook.color !== selectedPiece.color ||
+			rook.hasMoved
+		) {
+			return false;
+		}
+
+		const start = Math.min(fromCol, rookCol) + 1;
+		const end = Math.max(fromCol, rookCol);
+		for (let col = start; col < end; col++) {
+			if (board[fromRow][col]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	return false;
 }
 
 export default function isLegalMove(
-  lastMove,
-  board,
-  selectedPiece,
-  curPos,
-  targetPos
+	lastMove,
+	board,
+	selectedPiece,
+	curPos,
+	targetPos
 ) {
-  switch (selectedPiece.name) {
-    case PIECES.PAWN:
-      return pawnAttempt(lastMove, board, selectedPiece, curPos, targetPos);
-    case PIECES.ROOK:
-      return rookAttempt(board, selectedPiece, curPos, targetPos);
-    case PIECES.KNIGHT:
-      return knightAttempt(board, selectedPiece, curPos, targetPos);
-    case PIECES.BISHOP:
-      return bishopAttempt(board, selectedPiece, curPos, targetPos);
-    case PIECES.QUEEN:
-      return queenAttempt(board, selectedPiece, curPos, targetPos);
-    case PIECES.KING:
-      return kingAttempt(board, selectedPiece, curPos, targetPos);
-    default:
-      return false;
-  }
+	switch (selectedPiece.name) {
+		case PIECES.PAWN:
+			return pawnAttempt(lastMove, board, selectedPiece, curPos, targetPos);
+		case PIECES.ROOK:
+			return rookAttempt(board, selectedPiece, curPos, targetPos);
+		case PIECES.KNIGHT:
+			return knightAttempt(board, selectedPiece, curPos, targetPos);
+		case PIECES.BISHOP:
+			return bishopAttempt(board, selectedPiece, curPos, targetPos);
+		case PIECES.QUEEN:
+			return queenAttempt(board, selectedPiece, curPos, targetPos);
+		case PIECES.KING:
+			return kingAttempt(board, selectedPiece, curPos, targetPos);
+		default:
+			return false;
+	}
 }
